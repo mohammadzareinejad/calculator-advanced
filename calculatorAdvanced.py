308e584fa240041a84cfc53e4dce90f04d383087
darkener <zareiprogrammer@gmail.com>
2025-09-29 08:32:10 +0330

    create a unique list for avaliable  extensions

--- calculatorAdvanced.py
+++ calculatorAdvanced.py
@@ -14,6 +14,12 @@ if not os.path.exists(e):
     print('Folder {} not exists!'.format(e))
     sys.exit()
 
-    #Tادامه اسکریپت
+#ادامه اسکریپت
 print('Continuing Scripts')
 
+#! دریافت لیستی از محتویات فولدر کاربر
+dir_list = os.listdir(e)[1:]
+
+files_dict = {dir_list[i]: 0 for i in range(len(dir_list))}
+extensions = [dir_list[i].split('.')[1] for i in range(len(dir_list))]
+files_set = list(set(extensions))
